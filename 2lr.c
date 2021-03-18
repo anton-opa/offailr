782e64d7ae6a90239b96e4536a135ba4dcee6e30
user <user@ws1013402149.pm>
2021-03-18 12:42:55 +0500

    2lr

--- 2lr.c
+++ 2lr.c
@@ -5,8 +5,7 @@ int main ()
     float x;
     printf ("Enter x->") ;
     scanf ("%f", &x);
-    if (sin(x)>=-1./2 && sin(x)<=1./2 && acos(2*sin(x))>=0 && acos(2*sin(x))<=sqrt(acos(-1)/2.)
-  && sqrt(cos(pow(acos(2*sin(x)),2)))>=0 && sqrt(cos(pow(acos(2*sin(x)),2)))<=1)  
+    if (x>=acos(-1) && x<=acos(1))
     {
         double y=acos(2*sin(x));
         double z=sqrt(cos(y*y));
