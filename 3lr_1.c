db15967fd18a7266858bad00e4faa91611b4ad18
anton-opa <80451590+anton-opa@users.noreply.github.com>
2021-04-01 13:07:05 +0500

    Update 3lr_1.c

--- 3lr_1.c
+++ 3lr_1.c
@@ -2,28 +2,25 @@
 #include <math.h>
 int main()
 {
-float h,f,x=0;
-scanf("%f",&h);
-printf("%-s\t", "x");
-printf("%s\n", "f(x)");
-printf("%s\n","------------------------");
-if (h<0.0f)
-	return 0;
-do
-{
-	if((x-0.25)<=h)
-	{
-		printf("%-8.4f\t",x);
-		f=exp(sin(x));
-		printf("%8.4f\n",f);
-		x+=h;
-	}
-	else
-	{
-	printf("%-8.4f\t",x);
-	f=(exp(x))-(1/(sqrt(x)));
-	printf("%8.4f\n",f);
-	x+=h;
-	}
-}while(x>=0 && (x-0.5)<=h);
+		float h, f, i = 0, x = 0;
+		scanf("%f", &h);
+		printf("%-s\t", "x");
+		printf("%s\n", "f(x)");
+		printf("%s\n", "------------------------");
+		if (h < 0.0f)
+				return 0;
+		do
+		{
+				if (x <= 0.25)
+				{
+						f = exp(sin(x));
+				}
+				else
+				{
+						f = exp(x) - (1 / (sqrt(x)));
+				}
+				printf("%-8.4f\t%8.4f\n", x, f);
+				i += 1;
+				x = 0 + i * h;
+		} while (x >= 0 && x <= 0.5);
 }
