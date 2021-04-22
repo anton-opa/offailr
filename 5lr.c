7961a132638c025de691e2e2c686c130d133cd66
anton-opa <vyatkin_as@edu.surgu.ru>
2021-04-23 00:58:18 +0500

    nicaaa

--- 5lr.c
+++ 5lr.c
@@ -9,14 +9,21 @@ int w10 ( int n )
         printf("%u\t", x);
     }
 }
-int r10( int n)
+int r10(int n)
 {
     int x;
-    for ( int i = 0; )
+    if (n>0)
+    {
+        x = n % 10;
+        printf("%u\t",x);
+        return r10(n/10);
+    }
 }
 int main ()
 {
     int n;
     scanf("%u", &n);
     w10(n);
+    printf("\n");
+    r10(n);
 }
