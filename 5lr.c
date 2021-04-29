27a4322fe94dcbc25fc901112d282292ebb7ac6f
user <user@ws1013402149.pm>
2021-04-29 12:23:22 +0500

    ?

--- 5lr.c
+++ 5lr.c
@@ -1,38 +1,38 @@
-#include <stdio.h>
 #include <math.h>
-void w10 (int n)
-{   
-        int n1, x, r = 0;
-        n1 = n;
-        while( n )
-        {
-                n /= 10;
-                r++;
-        }
-        while(r > 0)
-        {
-                x = n1 / pow(10, r-1);
-                n1 -= x * pow(10, r-1);
-                printf("%u\t", x);
-                r--;
-        }
+#include <stdio.h>
+void w10(int n)
+{
+    int n1, x, r = 0;
+    n1 = n;
+    while (n)
+    {
+        n /= 10;
+        r++;
+    }
+    while (r > 0)
+    {
+        x = n1 / pow(10, r - 1);
+        n1 -= x * pow(10, r - 1);
+        printf("%u\t", x);
+        r--;
+    }
 }
-void r10 (int n)
-{   
-        if(n%10==n) 
-        {
-                printf("%u\t",n);
-                return;
-        }
-        r10( n / 10 );
-        printf("%u\t",n % 10);
+void r10(int n)
+{
+    if (n % 10 == n)
+    {
+        printf("%u\t", n);
+        return;
+    }
+    r10(n / 10);
+    printf("%u\t", n % 10);
 }
-int main ()
+int main()
 {
     int n;
     scanf("%u", &n);
     w10(n);
     printf("\n");
     r10(n);
+    printf("\n");
 }
-
--- 6lr.c
+++ 6lr.c
@@ -1,29 +1,50 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <time.h>
-void fill ( int n , int a [])
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int z = 0, x = 0;
+void fill(int l, int a[], int n[], int p[])
 {
-    for (  int i = 0; i < n ; i ++)
-    a [ i ] = rand () % 101 - 50;
-}
-void per (int a[], int p[], int n[] )
-{
-  for ( int i = 0; i < n ; i ++)
-  {
-      if 
-  }
-}
-int main ()
-{
-    srand (time(NULL));
-    int n ;
-    scanf ("%d" , &n ) ;
-    int A [ n ];
-    int P [];
-    int N [];
-    fill (n , A ) ;
-    for ( int i = 0; i < n ; i ++)
+    for (int i = 0; i < l; i++)
     {
-        printf ("%4d" , A[i]) ;
+        a[i] = rand() % 101 - 50;
     }
+    for (int i = 0; i < l; i++)
+    {
+        if (a[i] > 0)
+        {
+            p[z] = a[i];
+            z++;
+        }
+        if (a[i] < 0)
+        {
+            n[x] = a[i];
+            x++;
+        }
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    int n;
+    scanf("%d", &n);
+    int A[n];
+    int P[z];
+    int N[x];
+    fill(n, A, P, N);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%4d", A[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < z; i++)
+    {
+        printf("%4d", P[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < x; i++)
+    {
+        printf("%4d", N[i]);
+    }
+    printf("\n");
 }
